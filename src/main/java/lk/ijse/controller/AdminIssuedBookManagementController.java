53e5ca788ea24ed6d4f8bb4bad322c0ea6069209
rashiniashinsana <rashinivithanage2@gmail.com>
2024-03-12 01:31:33 +0530

    Completed FXML files part 2

--- src/main/java/lk/ijse/controller/AdminIssuedBookManagementController.java
+++ src/main/java/lk/ijse/controller/AdminIssuedBookManagementController.java
@@ -1,4 +1,92 @@
 package lk.ijse.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class AdminIssuedBookManagementController {
+
+    @FXML
+    private TextField UserId;
+
+    @FXML
+    private JFXButton btnAdd;
+
+    @FXML
+    private JFXButton btnClear;
+
+    @FXML
+    private JFXButton btnDelete;
+
+    @FXML
+    private JFXButton btnUpdate;
+
+    @FXML
+    private TableColumn<?, ?> colAmount;
+
+    @FXML
+    private TableColumn<?, ?> colBookId;
+
+    @FXML
+    private TableColumn<?, ?> colDueDate;
+
+    @FXML
+    private TableColumn<?, ?> colIssuedBookId;
+
+    @FXML
+    private TableColumn<?, ?> colIssuedDate;
+
+    @FXML
+    private TableColumn<?, ?> colReturnDate;
+
+    @FXML
+    private TableColumn<?, ?> colStatus;
+
+    @FXML
+    private TableColumn<?, ?> colUserId;
+
+    @FXML
+    private TableView<?> tblIssuedBooks;
+
+    @FXML
+    private TextField txtAmount;
+
+    @FXML
+    private TextField txtBookId;
+
+    @FXML
+    private TextField txtDueDate;
+
+    @FXML
+    private TextField txtIssueDate;
+
+    @FXML
+    private TextField txtReturnDate;
+
+    @FXML
+    private TextField txtStatus;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnclearOnAction(ActionEvent event) {
+
+    }
+
 }
